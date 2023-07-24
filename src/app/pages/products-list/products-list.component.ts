1100d5112673a5830b48c47bda2ed0fb4dddc64a
Letto228 <learnjs@MacBook-Pro-Egor.local>
2023-07-24 19:54:15 +0300

    feat: apply scroll with loading directive

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {productsMock} from '../../shared/products/products.mock';
 import {IProduct} from '../../shared/products/product.interface';
+import {LoadDirection} from '../../shared/scroll-with-loading/load-direction.const';
 
 @Component({
     selector: 'app-products-list',
@@ -23,8 +24,13 @@ export class ProductsListComponent implements OnInit {
 
     get productsFromGet(): IProduct[] | null {
         // eslint-disable-next-line no-console
-        console.log('Get products');
+        // console.log('Get products');
 
         return this.products;
+    }
+
+    onLoad(direction: LoadDirection) {
+        // eslint-disable-next-line no-console
+        console.log(`load ${direction}`);
     }
 }
