eca14d6111b97f6694e91fa12534ff0f72b8313a
Letto228 <learnjs@MacBook-Pro-Egor.local>
2023-08-01 09:28:07 +0300

    feat: add homework stand

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -20,6 +20,13 @@ export class ProductsListComponent implements OnInit {
     // );
     readonly products$ = this.productsStoreService.products$;
 
+    // for easy
+    name = 'Мышь';
+
+    // for hard
+    readonly propertyName = 'feedbacksCount' as const; // keyof IProduct
+    searchPropertyValue = 2;
+
     // constructor(private readonly changeDetectorRef: ChangeDetectorRef) {}
     // constructor(@Inject(ProductsStoreService) private readonly productsStoreService: ProductsStoreService) {}
     constructor(
